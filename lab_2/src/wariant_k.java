2e1ec4546ee3bf5b0488f97f49bb8b60693d0e76
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2023-10-16 12:31:32 +0200

    commit

--- lab_2/src/wariant_k.java
+++ lab_2/src/wariant_k.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
-public class A1 {
-    public static void main(String[] args){
+public class wariant_k {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Podaj pierwszą liczbe");
         int a = input.nextInt();
         System.out.println("Podaj drugą liczbe");
         int b = input.nextInt();
-        System.out.println("Dodawanie: " + (a+b));
 
+        while (a<=b){
+            if (a%2==0){
+                System.out.println(a);
+            }
+    a++;
+        }
     }
 }
