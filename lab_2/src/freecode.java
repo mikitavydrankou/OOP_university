6b7f3a3797fd86cee18321f9020f66175fbf17c1
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2023-10-16 12:55:25 +0200

    commit

---
+++ lab_2/src/freecode.java
@@ -0,0 +1,9 @@
+public class freecode {
+    public static void main(String[] args) {
+        System.out.println(suma(2,9));
+    }
+
+    public static int suma(int a, int b){
+        return a+b;
+    }
+}
