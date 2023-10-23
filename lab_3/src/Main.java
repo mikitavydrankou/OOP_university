f652a332d39abcf75a86deb883243615da3978c8
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2023-10-23 12:40:24 +0200

    commit

---
+++ lab_3/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double miles = 10.0; // Замените это значение на то, которое вы хотите конвертировать
+        double kilometers = wariant342.mileKilometry(150);
+        System.out.println(kilometers + " km");
+        double kilometers2 = wariant342.mileKilometry(200);
+        System.out.println(kilometers2 + " km");
+    }
+}
---
+++ lab_3/src/wariant342.java
@@ -0,0 +1,8 @@
+
+public class wariant342 {
+    public static double mileKilometry(double a){
+        double km = a / 0.621371;
+        return km;
+    }
+
+}
